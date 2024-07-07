b5fb31f3a74f8aab8a468ce5c1f0c3684ab27da4
pvpiv <fmdnyc@gmail.com>
2024-07-07 09:48:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,8 @@ if family_data:
     st.table(df_display)
 else:
     st.write("No data available for the selected options.")
-streamlit_analytics.stop_tracking()
+streamlit_analytics.track(save_to_json="analytics.json")
+#streamlit_analytics.stop_tracking()
 # Custom CSS to improve mobile view and table fit
 st.markdown(
     """
