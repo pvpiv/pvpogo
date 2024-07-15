f4eba6b7703e5badcf36191d8d2e222bbf800471
pvpiv <fmdnyc@gmail.com>
2024-07-15 14:18:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -109,32 +109,34 @@ else:
 
 pokemon_list = MyList(pokemon_list)            
 #pokemon_list = list(pokemon_list) + [""]
-pokemon_choice = st.selectbox('Select a Pokémon:', pokemon_list,index = pokemon_list.last_index())
 
-if pokemon_choice != "None" or pokemon_choice != "None (Shadow)":
-    #sel_pok = pokemon_choice
-    #pokemon_choice = pokemon_choice
-    #with streamlit_analytics.track():
-    #track = st.selectbox('Select a Pokémon:',pokemon_list,index = pokemon_list.index(sel_pok), label_visibility="hidden")
-
-    # Find the family of the selected Pokémon
-    pokemon_family = df[df['Pokemon'] == pokemon_choice]['Family'].iloc[0]
+if pokemon_list:
+    pokemon_choice = st.selectbox('Select a Pokémon:', pokemon_list,index = pokemon_list.last_index())
     
-    # Display formatted data for the selected Pokémon's family
-    family_data = format_data(pokemon_family, show_shadow)
-    if family_data:
+    if pokemon_choice != "None" or pokemon_choice != "None (Shadow)":
+        #sel_pok = pokemon_choice
+        #pokemon_choice = pokemon_choice
+        #with streamlit_analytics.track():
+        #track = st.selectbox('Select a Pokémon:',pokemon_list,index = pokemon_list.index(sel_pok), label_visibility="hidden")
+    
+        # Find the family of the selected Pokémon
+        pokemon_family = df[df['Pokemon'] == pokemon_choice]['Family'].iloc[0]
         
-        df_display = pd.DataFrame(family_data)
-        # Set up DataFrame for proper display
-        df_display.rename(columns={df.columns[0]: 'Pokemon'})
-        df_display.rename(columns={df.columns[1]: 'Attribute'})
-        df_display.set_index(['Pokemon'], inplace=True)
-        st.table(df_display)
-        save_new(streamlit_analytics.counts,"counts")
-        streamlit_analytics.stop_tracking()
-    else:
-        st.write("No data available for the selected options.")
-        streamlit_analytics.stop_tracking()
+        # Display formatted data for the selected Pokémon's family
+        family_data = format_data(pokemon_family, show_shadow)
+        if family_data:
+            
+            df_display = pd.DataFrame(family_data)
+            # Set up DataFrame for proper display
+            df_display.rename(columns={df.columns[0]: 'Pokemon'})
+            df_display.rename(columns={df.columns[1]: 'Attribute'})
+            df_display.set_index(['Pokemon'], inplace=True)
+            st.table(df_display)
+            save_new(streamlit_analytics.counts,"counts")
+            streamlit_analytics.stop_tracking()
+        else:
+            st.write("No data available for the selected options.")
+            streamlit_analytics.stop_tracking()
 
 #streamlit_analytics.track(save_to_json="analytics.json")
 #streamlit_analytics.track(firestore_key_file="firebase-key.json", firestore_collection_name="counts")
