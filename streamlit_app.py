71e6f212c3ca9f88193279e038157dfd37bb50ec
pvpiv <fmdnyc@gmail.com>
2024-07-15 14:42:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,7 +87,7 @@ def format_data(pokemon_family, shadow_only):
 if 'show_shadow' not in st.session_state:
     st.session_state.show_shadow = False
     
-if 'pokemon_choice' in st.session_state or 'pokemon_choice_new' in st.session_state::
+if 'pokemon_choice' in st.session_state or 'pokemon_choice_new' in st.session_state:
     if (pokemon_choice != 'Select a pokemon' and pokemon_choice != "Select a Shadow pokemon") or (pokemon_choice_new != 'Select a pokemon' and pokemon_choice_new != "Select a Shadow pokemon"):
         pokemon_choice_new = ""
         load_new(streamlit_analytics.counts,"counts")
