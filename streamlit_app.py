421c97f71e47f06678afa944d25c866ba20e393e
pvpiv <fmdnyc@gmail.com>
2024-07-15 12:03:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,40 +2,67 @@ import streamlit as st
 import pandas as pd
 import streamlit_analytics
 import json
+import base64
+import tempfile
 from google.cloud import firestore
 from google.oauth2 import service_account
-
+#counts = {"loaded_from_firestore": False}
 # Load your dataset
 df = pd.read_csv('pvp_data.csv')
 url = "https://pvpcalc.streamlit.app/"
 st.write("[Check CP for all IVs here](%s)" % url)
+# Define a function to format the data as required
+
+#fbase = st.secrets["fbase"]
+#fbase = json.dumps(fbase.to_dict())
+
+
+#key_dict = json.loads(st.secrets["textkey"])
+#creds = service_account.Credentials.from_service_account_info(key_dict)
+#db = firestore.Client(credentials=creds, project="Pvpogo")
+
 
 def load_new(counts, collection_name):
     """Load count data from firestore into `counts`."""
+
+    # Retrieve data from firestore.
     key_dict = json.loads(st.secrets["textkey"])
+    #creds = firestore.Client.from_service_account_json(key_dict)
     creds = service_account.Credentials.from_service_account_info(key_dict)
     db = firestore.Client(credentials=creds, project="pvpogo")
+   
     col = db.collection(collection_name)
     firestore_counts = col.document("counts").get().to_dict()
+
+    # Update all fields in counts that appear in both counts and firestore_counts.
     if firestore_counts is not None:
         for key in firestore_counts:
             if key in counts:
                 counts[key] = firestore_counts[key]
 
+
 def save_new(counts, collection_name):
     """Save count data from `counts` to firestore."""
     key_dict = json.loads(st.secrets["textkey"])
     creds = service_account.Credentials.from_service_account_info(key_dict)
+    #creds = firestore.Client.from_service_account_json(key_dict)
     db = firestore.Client(credentials=creds, project="pvpogo")
     col = db.collection(collection_name)
     doc = col.document("counts")
-    doc.set(counts)
+    doc.set(counts)  # creates if doesn't exist
+    
 
+#with open("/mount/src/pvpogo/cred.json", "w") as json_file:
+    #json_file.write(fbase)
+    
 def format_data(pokemon_family, shadow_only):
+    # Filter data for the family and shadow condition
     if shadow_only:
         family_data = df[(df['Family'] == pokemon_family) & (df['Shadow'] == True)]
     else:
         family_data = df[(df['Family'] == pokemon_family) & (df['Shadow'] == False)]
+    
+    # Prepare the data for display
     formatted_data = []
     attributes = ['Rank', 'CP', 'IVs', 'Level', 'MoveSet']
     leagues = ['Little', 'Great', 'Ultra', 'Master']
@@ -45,28 +72,20 @@ def format_data(pokemon_family, shadow_only):
             for league in leagues:
                 value = row[f'{league}_{attr}']
                 if pd.notna(value) and isinstance(value, (int, float)):
-                    entry[league] = f'{int(value):,}'
+                    entry[league] = f'{int(value):,}'  # Remove decimals and format as integer
                 else:
                     entry[league] = value if pd.notna(value) else ''
             formatted_data.append(entry)
     return formatted_data
 
-# Initialize session state for checkbox and selectbox
-if 'show_shadow' not in st.session_state:
-    st.session_state.show_shadow = False
+# Set up UI elements
+#streamlit_analytics.start_tracking(load_from_json='data/data.json')
+load_new(streamlit_analytics.counts,"counts")
+streamlit_analytics.start_tracking()
 
-if 'pokemon_choice' not in st.session_state:
-    st.session_state.pokemon_choice = ""
-if 'user_interacted' not in st.session_state:
-    st.session_state.user_interacted = False
-
-# Checkbox for shadow Pokémon
-show_shadow = st.checkbox('Show only Shadow Pokémon', value=st.session_state.show_shadow)
-
-# Update session state for show_shadow
-if show_shadow != st.session_state.show_shadow:
-    st.session_state.show_shadow = show_shadow
-    st.session_state.user_interacted = True
+st.write("### Pokémon Selection")
+show_shadow = st.checkbox('Show only Shadow Pokémon', False)
+#streamlit_analytics.track(save_to_json="analytics.json")
 
 # Filter the dropdown list based on the checkbox
 if show_shadow:
@@ -74,36 +93,29 @@ if show_shadow:
 else:
     pokemon_list = df[~df['Pokemon'].str.contains("Shadow")]['Pokemon'].unique()
 
-# Add an empty option for the selectbox
-pokemon_list = [""] + list(pokemon_list)
+pokemon_choice = st.selectbox('Select a Pokémon:', pokemon_list)
 
-# Selectbox for Pokémon selection
-pokemon_choice = st.selectbox('Select a Pokémon:', pokemon_list, index=0 if st.session_state.pokemon_choice == "" else pokemon_list.index(st.session_state.pokemon_choice))
+# Find the family of the selected Pokémon
+pokemon_family = df[df['Pokemon'] == pokemon_choice]['Family'].iloc[0]
 
-# Check if the selection is different and update user interaction flag
-if pokemon_choice != st.session_state.pokemon_choice:
-    st.session_state.user_interacted = True
-
-# Process data only if a valid Pokémon is selected and user has interacted
-if pokemon_choice and st.session_state.user_interacted:
-    st.session_state.pokemon_choice = pokemon_choice  # Now update the session state after confirming interaction
-    pokemon_family = df[df['Pokemon'] == st.session_state.pokemon_choice]['Family'].iloc[0]
-    family_data = format_data(pokemon_family, show_shadow)
-    if family_data:
-        df_display = pd.DataFrame(family_data)
-        df_display.rename(columns={df.columns[0]: 'Pokemon'})
-        df_display.rename(columns={df.columns[1]: 'Attribute'})
-        df_display.set_index(['Pokemon'], inplace=True)
-        st.table(df_display)
-    else:
-        st.write("No data available for the selected options.")
+# Display formatted data for the selected Pokémon's family
+family_data = format_data(pokemon_family, show_shadow)
+if family_data:
     
-    # Save and stop tracking analytics only if user interaction
-    save_new(streamlit_analytics.counts, "counts")
-    st.session_state.user_interacted = False
+    df_display = pd.DataFrame(family_data)
+    # Set up DataFrame for proper display
+    df_display.rename(columns={df.columns[0]: 'Pokemon'})
+    df_display.rename(columns={df.columns[1]: 'Attribute'})
+    df_display.set_index(['Pokemon'], inplace=True)
+    st.table(df_display)
+    save_new(streamlit_analytics.counts,"counts")
+else:
+    st.write("No data available for the selected options.")
+#streamlit_analytics.track(save_to_json="analytics.json")
+#streamlit_analytics.track(firestore_key_file="firebase-key.json", firestore_collection_name="counts")
 
-streamlit_analytics.stop_tracking(unsafe_password=st.secrets["pass"])
-
+#streamlit_analytics.stop_tracking(firestore_key_file="/mount/src/pvpogo/cred.json", firestore_collection_name="counts")
+#streamlit_analytics.stop_tracking(firestore_key_file="cred.json", firestore_collection_name="pvpogo")
 # Custom CSS to improve mobile view and table fit
 st.markdown(
     """
@@ -119,7 +131,7 @@ st.markdown(
             width: 100% !important;
             display: block;
             overflow-x: auto;
-            -webkit-overflow-scrolling: touch;
+            -webkit-overflow-scrolling: touch; /* Smooth scrolling for iOS */
         }
         .css-1i0h2kc table {
             width: 100%;
