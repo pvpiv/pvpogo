8fc5bdc096b452bbee1656535673f5cd0ea99110
pvpiv <fmdnyc@gmail.com>
2024-07-17 10:41:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -91,7 +91,7 @@ if 'last_sel' not in st.session_state:
     st.session_state['last_sel'] = "Blank"
     st.session_state['get_dat'] = False
 else:
-     if not st.session_state['get_dat'] and st.session_state['last_sel'] != "Blank":
+     if not st.session_state['get_dat'] and st.session_state['last_sel'] != "Blank" and st.session_state['last_sel'] is not None:
          st.session_state['get_dat'] = True
      
         #pokemon_choice_new = ""
@@ -122,7 +122,6 @@ if pokemon_list:
     #pokemon_choice = st.selectbox('Select a Pokémon:',pokemon_list,index = pokemon_list.last_index(), label_visibility = 'hidden',key="poke_choice")
     pokemon_choice = st.selectbox('Select a Pokémon:',pokemon_list,index = None, label_visibility = 'hidden',key="poke_choice")
     st.session_state['last_sel'] = pokemon_choice
-    if pokemon_choice is not None:
         
     if st.session_state['get_dat']:
         if pokemon_choice is not None:
