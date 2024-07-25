109eaa72be9f6d4f17d3805ffbb461b6e67538f4
pvpiv <fmdnyc@gmail.com>
2024-07-25 09:32:41 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -220,9 +220,9 @@ if pokemon_list:
         pokemon_choice = st.selectbox('Select a Pokémon:',pokemon_list,index = pokemon_list.last_index(), label_visibility = 'hidden',key="poke_choice",on_change = poke_search)
         
     if "analytics" in st.query_params:
-        st.experimental_set_query_params(dex=pokemon_choice,analytics = "on")
+        st.query_params.from_dict({"dex":pokemon_choice,"analytics":"on"})
     else:
-        st.experimental_set_query_params(dex=pokemon_choice)
+        st.query_params.from_dict({"dex":pokemon_choice})
             
     if st.session_state['get_dat']:
         if pokemon_choice is not None:
