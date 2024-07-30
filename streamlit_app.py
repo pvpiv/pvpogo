d0090692dca23f4a7897c6a6b1e0bc7f2802eee0
pvpiv <fmdnyc@gmail.com>
2024-07-29 22:40:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -116,7 +116,7 @@ if show_string:
     
     placeholderlil.text_input(label='Little League Top ' + str(st.session_state.top_num) + ' Search String:', value=make_search_string("little", st.session_state.top_num), disabled=True)
     #st_copy_to_clipboard(make_search_string("little", st.session_state.top_num))
-    st.code(make_search_string("little", st.session_state.top_num)
+    st.code(make_search_string("little", st.session_state.top_num))
     placeholdergrt.text_input(label='Great League Top ' + str(st.session_state.top_num) + ' Search String: (For most PVP IVs add &0-1attack)', value=make_search_string("great", st.session_state.top_num), disabled=True)
     placeholderult.text_input(label='Ultra League Top ' + str(st.session_state.top_num) + ' Search String: (For most PVP IVs add &0-1attack)', value=make_search_string("ultra", st.session_state.top_num), disabled=True)
     placeholdermstr.text_input(label='Master League Top ' + str(st.session_state.top_num) + ' Search String: (For BEST PVP IVs add &3-4*)', value=make_search_string("master", st.session_state.top_num), disabled=True)
