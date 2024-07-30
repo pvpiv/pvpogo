adf66fe0450bb17d0d52dae445e9495b73831ea6
pvpiv <fmdnyc@gmail.com>
2024-07-30 11:19:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,7 +90,7 @@ def get_top_50_ids(rank_column, league, top_n,fam):
     prefix = 'cp-500&' if league == 'little' else 'cp-1500&' if league == 'great' else 'cp-2500&' if league == 'ultra' else ''
     ids_string = prefix + ','.join(all_ids)
     st.write(all_ids_set)
-    st.write(top_df['Filtered_Evo_next'])
+    st.write(top_df)
     return ids_string.replace("&,", "&")
 
 # Generate search string based on league
