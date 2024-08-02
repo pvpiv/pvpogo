3869d8dc757a9288e0568347d6d4543fa1795a6d
pvpiv <fmdnyc@gmail.com>
2024-08-02 15:23:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -120,17 +120,17 @@ if show_string:
 
     fam_box = st.checkbox('Include pre-evolutions',value=True)
     iv_box = st.checkbox('Include IV Filter (Finds good IVs for 98% of Top performers)',value =  False)
-    
     top_nbox = st.number_input('Top', value=st.session_state.top_num, key='top_no', on_change=update_top_num, min_value=5, max_value=200, step=5)
-
+    placeholder = st.empty()
     load_from_firestore(streamlit_analytics.counts, st.secrets["fb_col"])
     streamlit_analytics.start_tracking()
-        
-    st.text_input(label=today.strftime("%m/%d/%y"), value='*Click string to show Copy button and Paste top ' + str(st.session_state.top_num) + ' into PokeGO*', label_visibility='hidden', disabled=True, key="sstring")
+    
+    st.text_input(label=today.strftime("%m/%d/%y"), value='*Click string to show Copy button and Paste Top ' + str(st.session_state.top_num) + ' into PokeGO*', label_visibility='hidden', disabled=True, key="sstring")
     #st.text_input(label=today.strftime("%m/%d/%y"), value='Results for Top ' + str(st.session_state.top_num), label_visibility='hidden', disabled=True, key="nstring")
     
     try:
-        
+        resnstring = placeholder.text_input(label=today.strftime("%m/%d/%y"), value='Results for Top ' + str(st.session_state.top_num), label_visibility='hidden', disabled=True, key="nstring")
+        resnstring = placeholder.empty()
         save_to_firestore(streamlit_analytics.counts, st.secrets["fb_col"])
         streamlit_analytics.stop_tracking(unsafe_password=st.secrets['pass'])
         
