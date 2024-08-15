1e0ecb074a4b8abf062a6bdd7197928b1336594e
pvpiv <fmdnyc@gmail.com>
2024-08-15 13:14:21 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,34 +64,45 @@ def format_data(pokemon_family, shadow_only):
 def filter_ids(row):
     current_id = row['ID']
     evo_next_list = row['Evo_Fam'].split(';')
-    
-    # Check if 'Shadow' is TRUE and append '&shadow' if so
+
+    # If the Shadow column is TRUE, append &shadow to the IDs
     if row['Shadow'] == "TRUE":
         current_id = f"{current_id}&shadow"
         evo_next_list = [f"{id}&shadow" for id in evo_next_list]
-        
+
     if str(current_id) in evo_next_list:
         position = evo_next_list.index(str(current_id))
         filtered_list = evo_next_list[:position + 1]
     else:
         filtered_list = evo_next_list
-    #st.write(filtered_list)    
+
     return list(filtered_list)
 
-def get_top_50_ids(rank_column, league, top_n,fam,iv_bool,all=False):
-    df_all = df.sort_values(by=rank_column)
-    df_all['ID'] = df_all.apply(lambda row: f"{row['ID']}&shadow" if row['Shadow'] == "TRUE" else row['ID'],axis=1)
+def get_top_50_ids(rank_column, league, top_n, fam, iv_bool, all=False):
+    # Modify the Shadow IDs in both df_all and df_filtered before filtering and sorting
+    df_all['ID'] = df.apply(
+        lambda row: f"{row['ID']}&shadow" if row['Shadow'] == "TRUE" else row['ID'],
+        axis=1
+    )
     
-    df_filtered = df.dropna(subset=[rank_column])
+    df_all['Evo_Fam'] = df_all.apply(
+        lambda row: ';'.join([f"{id}&shadow" if row['Shadow'] == "TRUE" else id for id in row['Evo_Fam'].split(';')]),
+        axis=1
+    )
+
+    # Filter and sort df_all
+    df_filtered = df_all.dropna(subset=[rank_column])
     df_filtered = df_filtered[df_filtered[rank_column] <= top_n]
+    top_df = df_filtered.sort_values(by=rank_column).drop_duplicates(subset=['ID'])
+    
+    seen = set()
     if fam:
-        df_filtered['Filtered_Evo_next'] = df_filtered.apply(filter_ids, axis=1)
-        all_ids_set = set([item for sublist in df_filtered['Filtered_Evo_next'] for item in sublist])
+        top_df['Filtered_Evo_next'] = top_df.apply(filter_ids, axis=1)
+        all_ids_set = set([item for sublist in top_df['Filtered_Evo_next'] for item in sublist])
         all_ids = df_all['ID'].astype(str).tolist()
-        seen = set()
         all_ids = [element for element in all_ids if element in all_ids_set and not (element in seen or seen.add(element))]
     else:
-        all_ids = df_filtered['ID'].astype(str).tolist()
+        all_ids = top_df['ID'].astype(str).tolist()
     if all:
         prefix = ''
     else:
