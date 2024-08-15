cded5ac99373348cd3f2d2f8e9b92ce647ae2fb6
pvpiv <fmdnyc@gmail.com>
2024-08-15 13:31:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -181,10 +181,11 @@ if show_string:
     except:
         pass
     if not show_fossil:    
-        
-        st.write('Little League Top ' + str(st.session_state.top_num) + ' Search String:')
-        st.code(make_search_string("little", st.session_state.top_num,fam_box,iv_box))
-
+        try:
+            st.write('Little League Top ' + str(st.session_state.top_num) + ' Search String:')
+            st.code(make_search_string("little", st.session_state.top_num,fam_box,iv_box))
+        except:
+            pass
         try:
             st.write('Great League Top ' + str(st.session_state.top_num) + ' Search String: (For most PVP IVs add &0-1attack)')
             st.code(make_search_string("great", st.session_state.top_num,fam_box,iv_box))
