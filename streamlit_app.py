4fa058478c718a71fea02af2226cec054825c21f
pvpiv <fmdnyc@gmail.com>
2024-08-18 01:03:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,11 @@ url = "https://pvpcalc.streamlit.app/"
 st.write("[Check CP for all IVs here](%s)" % url)
 #df = pd.read_csv('pvp_data.csv')
 show_fossil = st.checkbox('Catch Cup Rankings')
+show_new_season = st.checkbox('New Season Rankings', value=True)
 if not show_fossil:
     df = pd.read_csv('pvp_data.csv')
+else if show_new_season:
+    df = pd.read_csv('pvp_data_new.csv')
 else:
     df = pd.read_csv('pvp_data_catch.csv')
 
