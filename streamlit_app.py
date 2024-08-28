3ea77de1c0b9a7edfeb6a0418e79da9be95fc510
pvpiv <fmdnyc@gmail.com>
2024-08-28 09:43:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ if 1 != 0:
     def format_data(pokemon_family, shadow_only):
 
         if shadow_only == 'All':
-            family_data = df[(df['Family'] == pokemon_family)].sort_values(by=['ID','Shadow'])
+            family_data = df[(df['Family'] == pokemon_family)].sort_values(by=['Shadow','ID'])
         elif shadow_only == True:
             family_data = df[(df['Family'] == pokemon_family) & (df['Shadow'] == True)]
         elif shadow_only == False:
