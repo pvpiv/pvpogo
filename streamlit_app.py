e9122d77648b763480778e0bcffed7595d6a50e6
pvpiv <fmdnyc@gmail.com>
2024-10-04 12:57:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -287,7 +287,7 @@ if st.session_state.show_string:
 try:    
     load_from_firestore(streamlit_analytics.counts, st.secrets["fb_col"])
     streamlit_analytics.start_tracking()
-    
+    st.divider()  
     st.text_input(label="Feedback", key="fstring")
     #st.text_input(label=today.strftime("%m/%d/%y"), value='Results for Top ' + str(st.session_state.top_num), label_visibility='hidden', disabled=True, key="nstring")
 
