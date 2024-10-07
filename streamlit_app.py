0d984ffe09ff05c19b992c3d168252792d000d16
pvpiv <fmdnyc@gmail.com>
2024-10-07 18:33:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -282,7 +282,7 @@ if st.session_state.show_string:
             st.write('Master League Top ' + str(st.session_state.top_num) + ' Search String:')#: (For BEST PVP IVs add &3*,4*)')
             st.code(make_search_string("master", st.session_state.top_num,fam_box,iv_box,inv_box))
             query_params = st.experimental_get_query_params()
-         try:            
+        try:            
             st.write('All Leagues ' + str(st.session_state.top_num))
             st.code(make_search_string("all", st.session_state.top_num,fam_box,iv_box,inv_box,True))    
         except:
