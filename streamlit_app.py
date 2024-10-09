5a72132b0778753b5ae599c5de0e5d2f2a8f40c9
pvpiv <fmdnyc@gmail.com>
2024-10-09 02:01:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -156,7 +156,29 @@ if 1 != 0:
     def master_but():
         st.session_state["master_clicked"] = not st.session_state["master_clicked"]
       
+    def format_data_top(pokes_df, league_top,num_rank):
+
     
+
+    family_data = pokes_df
+    
+    formatted_data = []
+    attributes = ['Rank','IVs','CP', 'Level', 'MoveSet']
+    leagues = [league_top]
+    for _, row in family_data.iterrows():
+        #for attr in attributes:
+        for league in leagues:
+            #entry = {'Pokemon': row['Pokemon'], 'Attribute': attr}
+            entry = {'Pokemon': row['Pokemon'], 'League': league}
+            #for league in leagues:
+            for attr in attributes:
+                if attr == 'Rank':
+                    if int(value) <= num_rank:
+                        value = row[f'{league}_{attr}']
+                        #entry[league] = f'{int(value):,}' if pd.notna(value) and isinstance(value, (int, float)) else value if pd.notna(value) else ''
+                        entry[attr] = f'{int(value):,}' if pd.notna(value) and isinstance(value, (int, float)) else value if pd.notna(value) else ''
+            formatted_data.append(entry)
+    return formatted_data
     
     def calculate_days_since(xDate):
         # Define the date range
@@ -288,28 +310,53 @@ if st.session_state.show_string:
     
 
     if not st.session_state['show_custom']:    
-        #try:
-        st.write('Little League Top ' + str(st.session_state.top_num) + ' Search String:')#:')
-        st.code(make_search_string("little", st.session_state.top_num,fam_box,iv_box,inv_box))
-        st.button("Show Little Table", key='little_table',on_click = little_but)
-        #except:
-            #pass
+        try:
+            st.write('Little League Top ' + str(st.session_state.top_num) + ' Search String:')#:')
+            st.code(make_search_string("little", st.session_state.top_num,fam_box,iv_box,inv_box))
+            st.button("Show Little Table", key='little_table',on_click = little_but)
+            
+
+            if st.session_state.little_clicked:
+                    family_data_lit = format_data_top(df,'Little',st.session_state.top_num)
+                    df_display_lit = pd.DataFrame(family_data_lit)
+                    df_display_lit.set_index(['Pokemon'], inplace=True)
+                    st.table(df_display_lit)
+        
+        except:
+            pass
         try:
             st.write('Great League Top ' + str(st.session_state.top_num) + ' Search String:')#: (For most PVP IVs add &0-1attack)')
             st.code(make_search_string("great", st.session_state.top_num,fam_box,iv_box,inv_box))
             st.button("Show Great Table", key='great_table',on_click = great_but)
+            if st.session_state.great_clicked:
+                family_data_Great = format_data_top(df,'Great',st.session_state.top_num)
+                df_display_Great = pd.DataFrame(family_data_Great)
+                df_display_Great.set_index(['Pokemon'], inplace=True)
+                st.table(df_display_Great)
+        
         except:
             pass
         try:
             st.write('Ultra League Top ' + str(st.session_state.top_num) + ' Search String:')#:: (For most PVP IVs add &0-1attack)')
             st.code(make_search_string("ultra", st.session_state.top_num,fam_box,iv_box,inv_box))
             st.button("Show Ultra Table", key='ultra_table',on_click =  ultra_but)
+            if st.session_state.ultra_clicked:
+                family_data_ultra = format_data_top(df,'Ultra',st.session_state.top_num)
+                df_display_ultra = pd.DataFrame(family_data_ultra)
+                df_display_ultra.set_index(['Pokemon'], inplace=True)
+                st.table(df_display_ultra)
+        
         except:
             pass
         try:
             st.write('Master League Top ' + str(st.session_state.top_num) + ' Search String:')#: (For BEST PVP IVs add &3*,4*)')
             st.code(make_search_string("master", st.session_state.top_num,fam_box,iv_box,inv_box))
             st.button("Show Master Table", key='master_table',on_click = master_but)
+            if st.session_state.master_clicked:
+                family_data_master = format_data_top(df,'Master',st.session_state.top_num)
+                df_display_master = pd.DataFrame(family_data_master)
+                df_display_master.set_index(['Pokemon'], inplace=True)
+                st.table(df_display_master)
             query_params = st.experimental_get_query_params()
         except:
             pass
