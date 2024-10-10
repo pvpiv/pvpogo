cbaffe65a5f06947e746274c84f87ab2fa5f4678
pvpiv <fmdnyc@gmail.com>
2024-10-09 22:28:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -383,7 +383,7 @@ if st.session_state.show_string:
                 df_display_Great = pd.DataFrame(family_data_Great)
                 df_display_Great.set_index(['Pokemon'], inplace=True)
                 #st.table(df_display_Great)
-                st.dataframe(df_display_Great)    st.dataframe
+                st.dataframe(df_display_Great)  
             st.code(make_search_string("great", st.session_state.top_num,fam_box,iv_box,inv_box))
         except:
            pass
