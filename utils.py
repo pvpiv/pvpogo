dd2d1e65915dfcc4e1f36bf39d8ccb7c682e7f2f
pvpiv <fmdnyc@gmail.com>
2024-10-10 23:02:15 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -112,13 +112,13 @@ def get_top_50_ids(df, rank_column, league, top_n, fam, iv_bool, inv_bool, xl_va
 
 def make_search_string(df, league, top_n, fam, iv_b, inv_b,sho_xl_val, all_pre=False):
     if league == 'little':
-        return get_top_50_ids(df, 'Little_Rank', 'little', top_n, fam, iv_b, inv_b,sho_xl_val)
+        return get_top_50_ids(df, 'Little_Rank', 'little', top_n, fam, iv_b, inv_b, all_pre,sho_xl_val)
     elif league == 'great':
-        return get_top_50_ids(df, 'Great_Rank', 'great', top_n, fam, iv_b, inv_b,sho_xl_val)
+        return get_top_50_ids(df, 'Great_Rank', 'great', top_n, fam, iv_b, inv_b, all_pre,sho_xl_val)
     elif league == 'ultra':
-        return get_top_50_ids(df, 'Ultra_Rank', 'ultra', top_n, fam, iv_b, inv_b,sho_xl_val)
+        return get_top_50_ids(df, 'Ultra_Rank', 'ultra', top_n, fam, iv_b, inv_b, all_pre,sho_xl_val)
     elif league == 'master':
-        return get_top_50_ids(df, 'Master_Rank', 'master', top_n, fam, iv_b, inv_b,True)
+        return get_top_50_ids(df, 'Master_Rank', 'master', top_n, fam, iv_b, inv_b, all_pre,True)
     elif league == 'all':
         return (
             get_top_50_ids(df, 'Little_Rank', 'little', top_n, fam, iv_b, inv_b, all_pre,sho_xl_val)
