8273f34db96a81b6c29e2183089c0f190c67452b
pvpiv <fmdnyc@gmail.com>
2024-10-11 00:54:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ cols = st.columns((2,8,3))
 
 with cols[0]:
      with stylable_container(
-        key="blue_popover",
+        key="settings",
         css_styles="""
             button {
                 width: 150px;
@@ -75,7 +75,6 @@ with cols[0]:
     ):
       
         popover = st.popover("Settings",use_container_width =True)
-        popover.text_input(key='blue_popover')
         popover.subheader("Data Settings",divider = 'blue')
         show_custom_boxz = popover.checkbox('Sunshine Cup', on_change=upd_cust, key='sho_cust')
         show_shadow_boxz = popover.checkbox('Include Shadow Pokémon', on_change=upd_shadow, key='sho_shad', value=st.session_state['get_shadow'])
