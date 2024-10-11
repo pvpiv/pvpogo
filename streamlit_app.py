115a386668419266b3aa476ff4fa1263407f5cb7
pvpiv <fmdnyc@gmail.com>
2024-10-11 02:15:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,7 +90,7 @@ with cols[0]:
             fam_box = popover.checkbox('Include pre-evolutions', value=True)
             show_xl_boxz = popover.checkbox('Include XL Pokémon (No XL Candy needed)', on_change=upd_xl, key='sho_xl', value=st.session_state['show_xl'])
             iv_box = popover.checkbox('Include IV Filter (Works for Non XL Pokémon)', value=False)
-            popover.divider()
+            popover.subheader("Rankings Tables",divider="blue")
             popover.button("Show Little Table", key='little_table', on_click=little_but)
             popover.button("Show Great Table", key='great_table', on_click=great_but)
             popover.button("Show Ultra Table", key='ultra_table', on_click=ultra_but)
