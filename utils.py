e748bad8107d8b760a231fc2a55825bf0d423f7b
pvpiv <fmdnyc@gmail.com>
2024-10-24 13:52:26 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -56,14 +56,20 @@ def format_data(pokemon_family, shadow_only, df):
             formatted_data.append(entry)
     return formatted_data
 
+
+
 def filter_ids(row):
     current_id = row['ID']
-    evo_next_list = row['Evo_Fam'].split(';')
+    
+    # Split the Evo_Fam into a list and remove everything after '&'
+    evo_next_list = [elem.split('&')[0] for elem in row['Evo_Fam'].split(';')]
+    
     if str(current_id) in evo_next_list:
         position = evo_next_list.index(str(current_id))
         filtered_list = evo_next_list[:position + 1]
     else:
         filtered_list = evo_next_list
+
     return list(filtered_list)
 
 def get_top_50_ids(df, rank_column, league, top_n, fam, iv_bool, inv_bool, xl_var = True, all=False):
